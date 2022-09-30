d7d6205351d3bb9a4ed43e2d74518f8800f6a63f
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-30 20:25:55 +0000

    Finished BreedFetcher Activity: Fixed up Test File and added in a test for if an invalid breed is provided

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,26 +1,27 @@
+//First attempt: Creating a request function that is going to allow us to retrieve the information
+//we want about a cat breed.
+
 const fs = require('fs');
 
 const userSearch = process.argv.slice(2);
-
-console.log(userSearch);
 
 const request = require('request');
 
 let breedName = userSearch;
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log("Breed not found");
-  }
-});
+// request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+//   if (error) {
+//     console.log(error);
+//     return;
+//   }
+//   const data = JSON.parse(body);
+//   const breed = data[0];
+//   if (breed) {
+//     console.log(breed.description);
+//   } else {
+//     console.log("Breed not found");
+//   }
+// });
 
 //Some notes on what is going on up here:
 //We are requesting information from the CAT API. We adjust the URL so that it can bridge together with the
@@ -33,3 +34,23 @@ request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, res
 //However if you search for a breed that does not exist on the CAT API,
 //nothing is foudn which returns an empty array. IF no object sits in index 0 of that array,
 //than the breed will return as falsy and we will skip past to the else statement where it states BREED NOT FOUND.
+
+//Attempt 2: We are now going to refractor the code into a function.
+//
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("Breed not found", null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
